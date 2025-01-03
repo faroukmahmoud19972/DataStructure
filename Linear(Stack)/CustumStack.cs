2133d484c0fca97d1b60a35b5e8fadaabe80ffb6
farouk.mahmoud <farouk.mahmoud@FAROUK-MAHMOUD>
2025-01-03 18:09:49 +0200

    Add project files.

---
+++ Linear(Stack)/CustumStack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Stack_
+{
+    public class CustumStack<T>
+    {
+        public int index;
+        public List<T> List;
+        public CustumStack()
+        {
+            index = -1;   // when push item then add index -1+1 = 0
+            List = new List<T>();
+             
+        }
+        public int Count
+        {
+            get { return List.Count; }
+
+
+        }
+        public void Push (T item)
+        {
+            List.Add(item); 
+            index++;
+        }
+
+        // pop get the the last element then remove 
+        public T Pop ()
+        {
+            var last = List[index];
+            List.RemoveAt(index);
+            index--;
+            return last;
+        }
+        public T Peek()
+        {
+            var last = List[index];
+            return last;
+        }
+        public void Clear ()
+        {
+            List.Clear();
+        }
+    }
+}
---
+++ Linear(Stack)/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Linear_Stack_;
+
+CustumStack<int> custumStack = new();
+
+
+custumStack.Push(5); 
+custumStack.Push(6); 
+custumStack.Push(7);
+Console.WriteLine(custumStack.Count);
+
+Console.WriteLine(custumStack.Peek());
+
+
+Console.WriteLine(new string('*', 40));
+
+custumStack.Pop();
+
+
+
+foreach (var item in custumStack.List)
+{
+    Console.WriteLine(item);
+};
+
+
+
+Console.WriteLine(new string('*', 100));
+
+Stack<int> stack = new Stack<int>();
+
+stack.Push(5);
+
+stack.Peek();
+Console.WriteLine(stack.Count);
